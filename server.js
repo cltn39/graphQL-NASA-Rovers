6dd11b5f8336bac49351aad7d2fba94614c255a7
gravitation <chisong@ucdavis.edu>
2019-09-25 17:06:26 -0700

    working graphql-yoga

--- server.js
+++ server.js
@@ -1,39 +1,17 @@
-const { GraphQLserver } = require( 'graphql-yoga' );
-const { getRover } = require('./api');
+const { GraphQLServer } = require('graphql-yoga')
+ 
 const typeDefs = `
-type Query {
+  type Query {
     information: String
-    rover(roverName: Strong): Rover
-}
-
-type Rovers {
-    name: String
-    landingDate: string
-    launchDate: String
-    status: String
-    maxSole: Int
-    maxDate: String
-    totlaPhotos: Int
-    photos: [photo]
-}
-
-type Photo {
-    id: Int
-    sol: Int
-    src: String
-    earthDate: String
-}
-` 
-
+    rover(roverName: String): String
+  }
+`
+ 
 const resolvers = {
-    Query: {
-        information: () => "This is a graphql server",
-        rover: (root, args) => getRover(args.roverName)
-    }
+  Query: {
+    information: () => "This is a graphql server",
+  },
 }
-const server = new GraphQLserver ({
-    typeDefs,
-    resolvers
-})
-
+ 
+const server = new GraphQLServer({ typeDefs, resolvers })
 server.start({port:5000}, () => console.log('Server is running on localhost:5000'))
