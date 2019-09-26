217dcff225e88eb2411a14cee8d288b8ad913f82
gravitation <chisong@ucdavis.edu>
2019-09-25 17:19:05 -0700

    added api routs

---
+++ api.js
@@ -0,0 +1,34 @@
+const fetch = require('node-fetch');
+const API_KEY_UNSAFE = '4GMIiF8P5loAdqQiupM3cpYgDBzwYk10oe7rFUN'
+
+const getPhotos = async(roverName) => {
+    const photos = await fetch(``)
+    .then(data => data.json())
+    .then(res => res.photos)
+    return photos.map(photo => ({
+        id: photo.id,
+        sol: photo.sol,
+        sec: photo.img_src,
+        earthDate: photo.earth_date
+    }))
+}
+const getRover = async () => {
+    const rover = await fetch(`https://api.nasa.giv/planetary/apod?api_key=${API_KEY_UNSAFE}`)
+    .then(data => data.json())
+    .then(res => res.photo_manifest)
+
+    console.log(rover)
+
+    return ({
+        name: rover.name,
+        landingDate: rover.landing_date,
+        launchDate:  rover.launch_date,
+        status: rover.status,
+        maxSol: rover.max_sol,
+        maxDate: rover.max_date,
+        totlaPhotos: rover.total_photos,
+        photos: getPhotos(roverName)
+    })
+};
+
+module.exports = { getRover }
--- server.js
+++ server.js
@@ -1,16 +1,36 @@
-const { GraphQLServer } = require('graphql-yoga')
- 
+const { GraphQLServer } = require('graphql-yoga');
+const { getRover } = require('./api');
+
 const typeDefs = `
   type Query {
     information: String
-    rover(roverName: String): String
+    rover(roverName: String): Rovers
+  }
+
+  type Rovers {
+      name: String
+      landingDate: String
+      launchDate: String
+      status: String
+      maxSole: Int
+      maxDate: String
+      totalPhotos: Int
+      photos: [Photo]
+  }
+
+  type Photo {
+      id: Int
+      sol: Int
+      src: String
+      earthDate: String
   }
 `
  
 const resolvers = {
   Query: {
     information: () => "This is a graphql server",
-  },
+    rover: (root, args) => getRover(args.roverName)
+  }
 }
  
 const server = new GraphQLServer({ typeDefs, resolvers })
